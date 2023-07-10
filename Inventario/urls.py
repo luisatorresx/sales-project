9a605ba46b090c2714ccb0462fab714c9f8bdc30
DDVillegas <DDVillegas>
2023-07-10 19:57:42 -0400

    ultima modificacion por hoy 10/07/23

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= "index"),
+    path('', views.index, name= "index_inventario"),
     path('agregar_producto/', views.agregar_producto, name= "agregar_producto"),
     path('lista_productos/', views.lista_productos, name= "lista_productos"),
     path('reporte/', views.generar_reporte, name="generar_reporte"),
