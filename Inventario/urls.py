12962d8c844af197c367f91f978c447ad7c9a378
DDVillegas <krampuschief@gmail.com>
2023-07-14 19:00:58 -0400

    mas o menos funciona actualizar producto

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('lista_productos/', views.lista_productos, name= "lista_productos"),
     path('reporte/', views.generar_reporte, name="generar_reporte"),
     path('actualizar_producto/', views.actualizar_producto, name="actualizar_producto"),
+    path('error/', views.error, name="error"),
 ]
