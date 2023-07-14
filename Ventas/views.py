814ef3566411f996a32237f8508891dec658cc49
Andres-Goncalves <andres070599@gmail.com>
2023-07-14 09:17:40 -0400

    factura

--- Ventas/views.py
+++ Ventas/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from Inventario.models import Productos
 from .forms import FacturaForm
 
 # Create your views here.
@@ -8,13 +10,17 @@ def index(request):
 def facturacion(request):
     
     if request.method == "POST":
+        if "añadir" in request.POST:
+            productos = Productos.objects.all()
+            print(request.POST)
         form = FacturaForm(request.POST)
-        if form.is_valid():
-            factura = {'NumeroDeFactura': request.POST['NumeroDeFactura'], 'NombreCompleto': request.POST['NombreCompleto'], 'Cedula': request.POST['Cedula'], 'Total': request.POST['Total'], 'CodigoProducto': request.POST['CodigoProducto']}
-            return render(request, 'Ventas/FacturaAImprimir.html', {'factura': factura})
-        else:
-            form.full_clean()
-            return render(request, 'Ventas/Facturacion.html', {'form': form})
-    else:
-        form = FacturaForm()
+        #if form.is_valid():
+            #factura = {'NumeroDeFactura': request.POST['NumeroDeFactura'], 'NombreCompleto': request.POST['NombreCompleto'], 'Cedula': request.POST['Cedula'], 'Total': request.POST['Total'], 'CodigoProducto': request.POST['CodigoProducto']}
+            #return render(request, 'Ventas/FacturaAImprimir.html', {'factura': factura})
+        #else:
+            #form.full_clean()
         return render(request, 'Ventas/Facturacion.html', {'form': form})
+    else:
+        procutosFactura = Productos.objects.all()
+        form = FacturaForm()
+        return render(request, 'Ventas/Facturacion.html', {'form': form, 'procutosFactura': procutosFactura})
