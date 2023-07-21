806b7871b28ce7610a088c8109d9f4557a2cf42c
DDVillegas <krampuschief@gmail.com>
2023-07-21 18:02:30 -0400

    Prueben el modulo de inventario

--- Inventario/forms.py
+++ Inventario/forms.py
@@ -1,12 +1,15 @@
 from django import forms
+from decimal import *
 
 class ProductoForm(forms.Form):
-    nombre = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Nombre del producto"}))
+    nombre = forms.CharField(max_length=30,widget=forms.TextInput(attrs={"class": "textInput", "onkeydown":"return /[a-záéíóúñü]/i.test(event.key)", "autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "placeholder":"Nombre"}))
 
-    codigo = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "textInput", "placeholder": "Código del producto"}))
+    codigo = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "textInput", "min":"1", "step":"1","onkeydown":"if(event.key==='.'){event.preventDefault();}","autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "placeholder":"Código"}))
 
-    precio = forms.DecimalField(decimal_places=2, widget=forms.NumberInput(attrs={"class": "textInput", "placeholder": "Precio del producto"}))
+    precio = forms.DecimalField(required=False,max_digits=10, decimal_places=2, initial=Decimal(0.00).quantize(Decimal('.01')), widget=forms.NumberInput(attrs={"class": "textInput", "autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "onchange":"this.value = parseFloat(this.value).toFixed(2);", "placeholder":"Precio"}))
 
-    stock = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "textInput", "placeholder": "Stock disponible"}))
+    stock = forms.IntegerField(widget=forms.NumberInput(attrs={"class": "textInput", "min":"1", "step":"1","onkeydown":"if(event.key==='.'){event.preventDefault();}","autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "placeholder":"Unidades"}))
 
-    proveedor = forms.CharField(widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Nombre del proveedor"}))
+    proveedor = forms.CharField(max_length=30,widget=forms.TextInput(attrs={"class": "textInput", "onkeydown":"return /[a-záéíóúñü]/i.test(event.key)", "autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "placeholder":"Proveedor"}))
+
+    iva = forms.IntegerField(widget=forms.NumberInput(attrs={"'value': '16', class": "textInput", "min":"1", "step":"1","onkeydown":"if(event.key==='.'){event.preventDefault();}","autocomplete":"on", "onCopy":"return false", "onDrag":"return false", "onDrop":"return false", "onPaste":"return false", "placeholder": "IVA"}))
--- Usuario/forms.py
+++ Usuario/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(label=('Nombre de usuario'), max_length=50, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Introduzca nombre de usuario"}))
-    email = forms.EmailField(label=('Correo electrónico'), widget=forms.EmailInput(attrs={"class": "textInput", "placeholder": "Introduzca correo electrónico"}))
-    password = forms.CharField(label=('Contraseña'), widget=forms.PasswordInput(attrs={"class": "textInput", "placeholder": "Introduzca contraseña"}))
-    first_name = forms.CharField(label=('Nombre'), max_length=30, required=False, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Introduzca nombre"}))
-    last_name = forms.CharField(label=('Apellido'), max_length=150, required=False, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Introduzca apellido"}))
+    username = forms.CharField(label=('Nombre de usuario'), max_length=50, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Nombre de usuario"}))
+    email = forms.EmailField(label=('Correo electrónico'), widget=forms.EmailInput(attrs={"class": "textInput", "placeholder": "Correo electrónico"}))
+    password = forms.CharField(label=('Contraseña'), widget=forms.PasswordInput(attrs={"class": "textInput", "placeholder": "Contraseña"}))
+    first_name = forms.CharField(label=('Nombre'), max_length=30, required=False, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Nombres"}))
+    last_name = forms.CharField(label=('Apellido'), max_length=150, required=False, widget=forms.TextInput(attrs={"class": "textInput", "placeholder": "Apellidos"}))
 
     class Meta:
         model = User
