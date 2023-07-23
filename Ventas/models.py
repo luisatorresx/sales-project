8e4d7f8e1dcf579201d283124524b73e3bd2501d
Andres-Goncalves <andres070599@gmail.com>
2023-07-23 03:37:13 -0400

    Base de datos volatilizada

--- Ventas/models.py
+++ Ventas/models.py
@@ -4,29 +4,30 @@ from django.utils.timezone import now
 # Create your models here.
 
 class HistorialTipoDeCambio(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     cambio = models.DecimalField(max_digits=10, decimal_places=4, default= 0.0000)
     fecha = models.DateTimeField(default=now, editable=False)
 
 class Clientes(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     cedula = models.IntegerField(unique=True)
     nombre = models.CharField(max_length=30, blank=True, null=True)
     apellido = models.CharField(max_length=30, blank=True, null=True)
 
 class IdentificadorProductos(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     codigo = models.IntegerField()
 
 class HistorialProductos(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     iva = models.IntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
+    cantidad = models.IntegerField(default=0)
     producto = models.ForeignKey(IdentificadorProductos, on_delete=models.CASCADE)
 
 class Facturas(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     fecha = models.DateTimeField(default=now, editable=False)
     total_base = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     iva = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
@@ -34,6 +35,8 @@ class Facturas(models.Model):
     impuesto_divisa = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     cancelado_en_bs = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     total = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
+    vuelto = models.DecimalField(max_digits=10, decimal_places=2, default= 0.00)
     id_cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
     id_tipo_de_cambio = models.ForeignKey(HistorialTipoDeCambio, on_delete=models.CASCADE)
     productos = models.ManyToManyField(HistorialProductos)
+    
