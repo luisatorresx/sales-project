cc1dbba303a3e8f37985691f63f4cc6506797f73
Andres-Goncalves <andres070599@gmail.com>
2023-07-26 16:57:20 -0400

    añadida base de datos a admin

--- Ventas/admin.py
+++ Ventas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Clientes)
+admin.site.register(models.HistorialProductos)
+admin.site.register(models.Facturas)
+admin.site.register(models.IdentificadorProductos)
+admin.site.register(models.HistorialTipoDeCambio)
